b19897d4ed28569b29f189a6cfc85c53aa20ba41
Podolyakin <podolyakionfwork@gmail.com>
2022-11-03 21:32:09 +0300

    Initial Commit

---
+++ User/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  User
+//
+//  Created by Сергей Подолякин on 3/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
