7401af3cf254b2c9dd09e65fab79a4a6fa9b7bd8
Podolyakin <podolyakionfwork@gmail.com>
2022-11-04 16:09:48 +0300

    work w/ login view controller

---
+++ User/LoginViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  User
+//
+//  Created by Сергей Подолякин on 3/11/22.
+//
+
+import UIKit
+
+class LoginViewContoller: UIViewController {
+
+    @IBOutlet weak var loginTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    @IBAction func loginButtonPressed(_ sender: UIButton) {
+        loginTextField.text == UserName.shared.login && passwordTextField.text == UserName.shared.password ? performSegue(withIdentifier: "showWelcome", sender: nil) : showAlert(with: "Wrong password", "Enter correct data")
+    }
+    
+    @IBAction func forgetPasswordButtonPressed(_ sender: UIButton) {
+        showAlert(with: "Your password here!", "120012")
+    }
+    
+    private func showAlert(with title: String, _ message: String, _ textField: UITextField? = nil) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "Okay", style: .default) { _ in
+            textField?.text = ""
+        }
+        alert.addAction(okAction)
+        present(alert, animated: true)
+    }
+}
+
---
+++ User/User.swift
@@ -0,0 +1,15 @@
+//
+//  User.swift
+//  User
+//
+//  Created by Сергей Подолякин on 3/11/22.
+//
+
+import Foundation
+
+struct UserName {
+    static let shared = UserName()
+    
+    let login = "podolyakinqq"
+    let password = "120012"
+}
